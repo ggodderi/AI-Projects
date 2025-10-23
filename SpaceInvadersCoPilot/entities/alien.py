47d7f9e74073d019d2085843b73b4182b4ac515f
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-10-22 18:19:34 -0600

    Update alien shapes.

--- SpaceInvadersCoPilot/entities/alien.py
+++ SpaceInvadersCoPilot/entities/alien.py
@@ -13,16 +13,26 @@ class Alien:
         self.alive = True
 
     def draw(self, surface: pygame.Surface) -> None:
-        # Draw a simple pixel-art alien (classic shape) with color
+        # Draw a spider-like alien creature
         x, y, w, h = self.rect.x, self.rect.y, self.rect.width, self.rect.height
         c = self.color
-        # Body
-        pygame.draw.rect(surface, c, (x + w // 8, y + h // 4, w * 3 // 4, h // 2))
-        # Eyes
-        pygame.draw.rect(surface, (0,0,0), (x + w // 4, y + h // 3, w // 8, h // 8))
-        pygame.draw.rect(surface, (0,0,0), (x + w - w // 4 - w // 8, y + h // 3, w // 8, h // 8))
-        # Antennae
-        pygame.draw.rect(surface, c, (x + w // 3, y, w // 6, h // 6))
-        # Legs
-        pygame.draw.rect(surface, c, (x + w // 8, y + h - h // 6, w // 8, h // 6))
-        pygame.draw.rect(surface, c, (x + w - w // 4, y + h - h // 6, w // 8, h // 6))
+        # Body (oval)
+        pygame.draw.ellipse(surface, c, (x + w // 8, y + h // 4, w * 3 // 4, h // 2))
+        # Eyes (black)
+        pygame.draw.circle(surface, (0,0,0), (x + w // 3, y + h // 2), w // 10)
+        pygame.draw.circle(surface, (0,0,0), (x + w - w // 3, y + h // 2), w // 10)
+        # Fangs (white)
+        pygame.draw.rect(surface, (255,255,255), (x + w // 2 - w // 12, y + h // 2 + h // 6, w // 24, h // 8))
+        pygame.draw.rect(surface, (255,255,255), (x + w // 2 + w // 24, y + h // 2 + h // 6, w // 24, h // 8))
+        # Legs (8 legs)
+        leg_color = (40,40,40)
+        leg_len = w // 3
+        for i in range(4):
+            # Left legs
+            start = (x + w // 4, y + h // 2 + i * h // 10)
+            end = (start[0] - leg_len, start[1] + leg_len // 2)
+            pygame.draw.line(surface, leg_color, start, end, 2)
+            # Right legs
+            start = (x + w - w // 4, y + h // 2 + i * h // 10)
+            end = (start[0] + leg_len, start[1] + leg_len // 2)
+            pygame.draw.line(surface, leg_color, start, end, 2)
